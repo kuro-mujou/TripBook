800feb80a5c3772a0984596e3cf01397dc03b15a
kuro-mujou <mguyenmanhtuan@gmail.com>
2024-04-23 15:40:04 +0700

    test UI compose

--- app/src/main/java/com/example/tripbook/MainActivity.kt
+++ app/src/main/java/com/example/tripbook/MainActivity.kt
@@ -3,44 +3,93 @@ package com.example.tripbook
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.tripbook.ui.theme.TripBookTheme
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TripBookTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+            val painter = painterResource(id = R.drawable.mainafter)
+            val contentDescription = "iguana"
+            val title = "iguana blue"
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Magenta)
+            ){
+                ImageCard(
+                    modifier = Modifier,
+                    painter = painter,
+                    contentDescription = contentDescription,
+                    title = title
+                )
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun ImageCard(
+    modifier: Modifier = Modifier,
+    painter: Painter,
+    contentDescription: String,
+    title: String
+){
+    Card(
         modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TripBookTheme {
-        Greeting("Android")
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(25.dp),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 20.dp
+        )
+    ){
+        Box(
+            modifier = Modifier.height(200.dp)
+        ){
+            Image(painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
+            )
+            Box(
+                modifier = Modifier.fillMaxSize()
+                    .background(
+                        Brush.verticalGradient(
+                            colors = listOf(Color.Red, Color.Green, Color.Blue)
+                        )
+                    )
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                contentAlignment = Alignment.BottomStart
+            ){
+                Text(
+                    title,
+                    style = TextStyle(Color.White, fontSize = 13.sp)
+                )
+            }
+        }
     }
 }
