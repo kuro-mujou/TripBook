0a0586d1d3961f380af210fd153e713e63fa2c53
kuro-mujou <mguyenmanhtuan@gmail.com>
2024-04-24 15:37:51 +0700

    setup UI color

--- app/src/main/java/com/example/tripbook/MainActivity.kt
+++ app/src/main/java/com/example/tripbook/MainActivity.kt
@@ -3,16 +3,23 @@ package com.example.tripbook
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -25,30 +32,73 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.tripbook.ui.theme.TripBookTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val painter = painterResource(id = R.drawable.mainafter)
-            val contentDescription = "iguana"
-            val title = "iguana blue"
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(Color.Magenta)
-            ){
-                ImageCard(
-                    modifier = Modifier,
-                    painter = painter,
-                    contentDescription = contentDescription,
-                    title = title
-                )
+            TripBookTheme {
+                Surface {
+                    Modifier.fillMaxSize()
+                    Box(modifier = Modifier.fillMaxSize())
+                }
+//                val painter = painterResource(id = R.drawable.mainafter)
+//                val contentDescription = "iguana"
+//                val title = "iguana blue"
+//                Column(
+//                    modifier = Modifier
+//                        .fillMaxSize()
+//                        .padding(50.dp),
+//                    horizontalAlignment = Alignment.CenterHorizontally,
+//                    verticalArrangement = Arrangement.Center
+//                ){
+//
+//                    Card(
+//                        modifier = Modifier.fillMaxSize()
+//                    ){
+//                        Box(
+//                            modifier = Modifier
+//                                .fillMaxSize(),
+////                                .background(Color(0xFFA06CB6)),
+//                            contentAlignment = Alignment.Center
+//                        ){
+//                            MyButton {}
+//                        }
+//                    }
+////                ImageCard(
+////                    modifier = Modifier,
+////                    painter = painter,
+////                    contentDescription = contentDescription,
+////                    title = title
+////                )
+//                }
             }
         }
     }
 }
+@Composable
+fun MyButton(onClick: () -> Unit) {
+    Button(
+        onClick = { onClick() },
+//        border = BorderStroke(
+//            width = 2.dp,
+//            color = Color.Black
+//        ),
+        colors = ButtonDefaults.buttonColors(
+                Color(0xFF6758C1)
+        ),
+        modifier = Modifier.size(width = 200.dp,height = 50.dp)
+    ) {
+        Text(
+            "???????????",
+            style = TextStyle(
+                color = Color.Black
+            )
+        )
 
+    }
+}
 @Composable
 fun ImageCard(
     modifier: Modifier = Modifier,
@@ -72,7 +122,8 @@ fun ImageCard(
                 contentScale = ContentScale.Crop
             )
             Box(
-                modifier = Modifier.fillMaxSize()
+                modifier = Modifier
+                    .fillMaxSize()
                     .background(
                         Brush.verticalGradient(
                             colors = listOf(Color.Red, Color.Green, Color.Blue)
--- app/src/main/java/com/example/tripbook/ui/theme/Theme.kt
+++ app/src/main/java/com/example/tripbook/ui/theme/Theme.kt
@@ -16,25 +16,91 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
+    /*
+        primary = primary_light,
+        onPrimary = onPrimary_light,
+        primaryContainer = primaryContainer_light,
+        onPrimaryContainer = onPrimaryContainer_light,
+        inversePrimary = inversePrimary_light,
+        secondary = secondary_light,
+        onSecondary = onSecondary_light,
+        secondaryContainer = secondaryContainer_light,
+        onSecondaryContainer = onSecondaryContainer_light,
+        tertiary = tertiary_light,
+        onTertiary = onTertiary_light,
+        tertiaryContainer = tertiaryContainer_light,
+        onTertiaryContainer = onTertiaryContainer_light,
+        background = background_light,
+        onBackground = onBackground_light,
+        surface = surface_light,
+        onSurface = onSurface_light,
+        surfaceVariant = surfaceVariant_light,
+        onSurfaceVariant = onSurfaceVariant_light,
+        surfaceTint = surfaceTint_light,
+        inverseSurface = inverseSurface_light,
+        inverseOnSurface = inverseOnSurface_light,
+        error = error_light,
+        onError = onError_light,
+        errorContainer = errorContainer_light,
+        onErrorContainer = onErrorContainer_light,
+        outline = outline_light,
+        outlineVariant = outlineVariant_light,
+        scrim = scrim,
+        surfaceBright = surfaceBright_light,
+        surfaceContainer = surfaceContainer_light,
+        surfaceContainerHigh = surfaceContainerHigh_light,
+        surfaceContainerHighest = surfaceContainerHighest_light,
+        surfaceContainerLow = surfaceContainerLow_light,
+        surfaceContainerLowest = surfaceContainerLowest_light,
+        surfaceDim = surfaceDim_light
+    * */
     primary = Purple80,
     secondary = PurpleGrey80,
     tertiary = Pink80
 )
 
 private val LightColorScheme = lightColorScheme(
+    /*
+        primary = primary_dark,
+        onPrimary = onPrimary_dark,
+        primaryContainer = primaryContainer_dark,
+        onPrimaryContainer = onPrimaryContainer_dark,
+        inversePrimary = inversePrimary_dark,
+        secondary = secondary_dark,
+        onSecondary = onSecondary_dark,
+        secondaryContainer = secondaryContainer_dark,
+        onSecondaryContainer = onSecondaryContainer_dark,
+        tertiary = tertiary_dark,
+        onTertiary = onTertiary_dark,
+        tertiaryContainer = tertiaryContainer_dark,
+        onTertiaryContainer = onTertiaryContainer_dark,
+        background = background_dark,
+        onBackground = onBackground_dark,
+        surface = surface_dark,
+        onSurface = onSurface_dark,
+        surfaceVariant = surfaceVariant_dark,
+        onSurfaceVariant = onSurfaceVariant_dark,
+        surfaceTint = surfaceTint_dark,
+        inverseSurface = inverseSurface_dark,
+        inverseOnSurface = inverseOnSurface_dark,
+        error = error_dark,
+        onError = onError_dark,
+        errorContainer = errorContainer_dark,
+        onErrorContainer = onErrorContainer_dark,
+        outline = outline_dark,
+        outlineVariant = outlineVariant_dark,
+        scrim = scrim_dark,
+        surfaceBright = surfaceBright_dark,
+        surfaceContainer = surfaceContainer_dark,
+        surfaceContainerHigh = surfaceContainerHigh_dark,
+        surfaceContainerHighest = surfaceContainerHighest_dark,
+        surfaceContainerLow = surfaceContainerLow_dark,
+        surfaceContainerLowest = surfaceContainerLowest_dark,
+        surfaceDim = surfaceDim_dark
+    * */
     primary = Purple40,
     secondary = PurpleGrey40,
     tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
 )
 
 @Composable
@@ -47,7 +113,10 @@ fun TripBookTheme(
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+            if (darkTheme)
+                dynamicDarkColorScheme(context)
+            else
+                dynamicLightColorScheme(context)
         }
 
         darkTheme -> DarkColorScheme
