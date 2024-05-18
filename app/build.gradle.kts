c08a2c0eca752be1a8e370618de616bd5fb889d5
Hieu411 <huahieu.cv@gmail.com>
2024-05-18 11:27:35 +0700

    HomePageLayout UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,7 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.material.icons.extended)
     implementation(libs.library.sync)
+    implementation(libs.androidx.benchmark.macro)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
