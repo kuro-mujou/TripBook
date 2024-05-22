9d6cbe83e79f9caa6fb8e8352c76a31ec35aab0d
Hieu411 <huahieu.cv@gmail.com>
2024-05-22 11:30:37 +0700

    Navigation2222

--- app/src/main/java/com/example/tripbook/navigationControl/MainNestedGraph.kt
+++ app/src/main/java/com/example/tripbook/navigationControl/MainNestedGraph.kt
@@ -6,8 +6,12 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
+import com.example.tripbook.ui.activityLayouts.mainRoute.CommingSoon.LoadingLayout
 import com.example.tripbook.ui.activityLayouts.mainRoute.HomePageLayout
 import com.example.tripbook.ui.activityLayouts.mainRoute.HomeViewModel
+import com.example.tripbook.ui.activityLayouts.mainRoute.HotelBookingRoute.RoomAndGuestLayout
+import com.example.tripbook.ui.activityLayouts.mainRoute.TransportsBookingRoute.TransportBookingLayout
+import com.example.tripbook.ui.activityLayouts.mainRoute.TripRoute.TripLayout
 
 fun NavGraphBuilder.mainGraph(navController: NavController){
     navigation(startDestination = Layouts.HomePageRoute.route,route = Layouts.MainRoute.route)
